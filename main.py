df9066477657970d695f14257dfec2a263a7b9e4
Roman <roman.semenikhin99@gmail.com>
2022-01-31 21:45:08 +0200

    reply murkup buttons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import config as cfg
 from datetime import date
 from dateutil.relativedelta import relativedelta, SU
 import telebot
+from telebot import types
 import json
 
 bot = telebot.TeleBot(cfg.token)
@@ -11,6 +12,23 @@ def get_last_sunday():
     today = date.today()
     last_monday = today+relativedelta(weekday=SU(-1))
     return last_monday.strftime("%d.%m.%Y")
+
+# buttons
+mm = types.ReplyKeyboardMarkup(row_width=2)
+button1  = types.KeyboardButton("Алтай")
+button2  = types.KeyboardButton("Пабло")
+button3  = types.KeyboardButton("Пино")
+button4  = types.KeyboardButton("Птица")
+button5  = types.KeyboardButton("Дёс")
+button6  = types.KeyboardButton("Тайлер")
+button7  = types.KeyboardButton("Жаба")
+button8  = types.KeyboardButton("Настя")
+button9  = types.KeyboardButton("Майки")
+button10 = types.KeyboardButton("Влад")
+button11 = types.KeyboardButton("Литр")
+button12 = types.KeyboardButton("Ювелир")
+button13 = types.KeyboardButton("Док")
+mm.add(button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13)
 
 @bot.message_handler(content_types=["text"])
 def main(message):
@@ -25,7 +43,7 @@ def main(message):
     user = list_obj[-2]["message_text"]
     msg = "\n"+str(list_obj[-1]["message_text"])+"\n\n\nПользователь: "+user+"\nКурьер ... от "+get_last_sunday()
 
-    bot.send_message(message.chat.id, msg)
+    bot.send_message(message.chat.id, msg,reply_markup=mm)
 
 if __name__ == '__main__':
     bot.infinity_polling() 
